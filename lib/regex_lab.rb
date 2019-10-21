001d0f49bbebed13edda72de2924e2273f3901a6
vy vo <tuonggvy.vo@gmail.com>
2019-10-21 23:22:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,8 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiou]/i) == nil
-    return false
-  else
+  if word.match(/^[aeiou]/i) == true
     return true
+  else
+    return false
   end
 end
 
