d044aa07cf211dbb5a67485cb99cb13b1d7c49c7
vy vo <tuonggvy.vo@gmail.com>
2019-10-21 23:51:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  #/[0-9]{10}/
   if phone.match(/\(?([0-9]{3}\)?([ -]?)([0-9]{3})( [-?])([0-9]{4}/) == nil
       return false
   else
