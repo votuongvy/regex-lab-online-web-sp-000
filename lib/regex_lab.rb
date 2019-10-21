03c7e2e2771cc60c6be3d29f5e879cd8ed605ecc
vy vo <tuonggvy.vo@gmail.com>
2019-10-21 23:27:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  #/^[A-Z][a-z]+!$/
+  if text.match(/^[A-Z]\w+!$) == nil
+    return false
+  else
+    return true
 end
 
 def valid_phone_number?(phone)
