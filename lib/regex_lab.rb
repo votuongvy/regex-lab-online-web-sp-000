a8fb57b5a59e55716def5bb0bdcb0ed6006f192a
vy vo <tuonggvy.vo@gmail.com>
2019-10-21 23:34:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z]\w+.*\.$/) == nil
+  if text.match(/^[A-Z].*\.$/) == nil
     return false
   else
     return true
